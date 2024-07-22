58c45e6384041be990d71668975d8e09ca21ada4
Long Nguyen <nguyendailong2k@gmail.com>
2024-07-22 21:31:14 +0700

    feat: init source

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LND's Blog</title>
+    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            
+        </div>
+        <div id="topic">
+
+        </div>
+    </div>
+    <script src="./js/main.js"></script>
+</body>
+</html>
