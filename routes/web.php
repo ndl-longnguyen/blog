cb0b16006b0a8aea63bf98aa6ee268f0cd5052ba
Long Nguyen <longnguyen@192.168.102.8>
2024-07-22 22:16:07 +0700

    feat: update webhook event

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
+Route::post('/', function (Request $request) {
+    // Xử lý webhook từ GitHub
+    $payload = json_decode($request->getContent(), true);
+
+    // Kiểm tra sự kiện push từ GitHub
+    if ($request->header('X-GitHub-Event') === 'push') {
+        // Lấy thông tin branch từ payload
+        $branch = str_replace('refs/heads/', '', $payload['ref']);
+
+        // Xác định repository và thực hiện pull code
+        $repositoryPath = base_path(); // Đường dẫn tới thư mục gốc của Laravel
+        $gitCommand = "cd {$repositoryPath} && git pull origin {$branch}";
+        $payload['repositoryPath'] = $repositoryPath;
+        $payload['gitCommand'] = $gitCommand;
+
+        // Thực thi lệnh git pull để cập nhật code
+        // $output = shell_exec($gitCommand);
+
+        // Log lại kết quả của lệnh git pull (optional)
+        // \Log::info("Git pull command output: {$output}");
+
+        DB::table("blogs")->insert([
+            "content" => $payload
+        ]);
+
+        // Trả về response nếu cần
+        return response()->json(['message' => 'Code pulled successfully'], 200);
+    }
+
+    // Trả về response nếu không phải sự kiện push
+    return response()->json(['message' => 'Unsupported event'], 400);
+
+   
+   
+    // return response()->json($data);
+});
+
+Route::get('/get-webhook-github', function (Request $request) {
+    $data = DB::table("blogs")->first();
+    return response()->json($data);
 });
